df33d9b42085553384bfab80c360f6955ebd4481
ty-hongjy <hongjy1@163.com>
2019-07-12 21:18:51 +0800

    20190712

--- shapeshed-nodejsbook.io.examples/hour13/example07/app.js
+++ shapeshed-nodejsbook.io.examples/hour13/example07/app.js
@@ -46,7 +46,7 @@ io.sockets.on('connection', function (socket) {
 
 // Configuration
 
-app.configure(function(){
+//app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
@@ -54,7 +54,7 @@ app.configure(function(){
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
-});
+//});
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));

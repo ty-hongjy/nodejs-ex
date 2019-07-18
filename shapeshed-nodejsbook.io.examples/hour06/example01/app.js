4a7f7d55fba3ed14608a7fa4b460ebc60ae84740
ty-hongjy <hongjy1@163.com>
2019-07-18 10:25:23 +0800

    20190718

---
+++ shapeshed-nodejsbook.io.examples/hour06/example01/app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+  routes = require('./routes'),
+  user = require('./routes/user'),
+  http = require('http'),
+  path = require('path'),
+  bodyParser = require('body-parser');  
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+
+// parse application/x-www-form-urlencoded  
+app.use(bodyParser.urlencoded({ extended: false }))  
+// parse application/json  
+app.use(bodyParser.json())  
+//app.use(express.bodyParser());
+
+//app.use(express.methodOverride());
+//app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' === app.get('env')) {
+  //app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});

315b2fa184bb55204f9b230c18734e33a6a9e3e5
ty-hongjy <hongjy1@163.com>
2019-07-18 16:40:21 +0800

    20190718

--- shapeshed-nodejsbook.io.examples/hour13/example01/app.js
+++ shapeshed-nodejsbook.io.examples/hour13/example01/app.js
@@ -1,18 +1,19 @@
 var express = require('express'),
   app = express(),
   server = require('http').createServer(app),
-  io = require('socket.io').listen(server);
+  io = require('socket.io').listen(server),
+  bodyParser = require('body-parser'),
+  errorHandler = require('errorhandler');
 
 app.set('port', process.env.PORT || 3000);
 
 if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
-
 
 io.sockets.on('connection', function (socket) {
   socket.emit('welcome', { text: 'OH HAI! U R CONNECTED!' });

90e078ea74725368b66c5d59dc81b80886cf467e
ty-hongjy <hongjy1@163.com>
2019-07-19 15:31:24 +0800

    20190719

--- shapeshed-nodejsbook.io.examples/hour13/example03/app.js
+++ shapeshed-nodejsbook.io.examples/hour13/example03/app.js
@@ -2,6 +2,7 @@ var express = require('express'),
   app = express(),
   server = require('http').createServer(app),
   io = require('socket.io').listen(server),
+  //errorHandler = require('errorhandler'),
   nicknames = [];
 
 app.set('port', process.env.PORT || 3000);

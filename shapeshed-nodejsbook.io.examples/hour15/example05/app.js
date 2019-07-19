23d17f0663a62d264566def38c85a0633e051ef0
ty-hongjy <hongjy1@163.com>
2019-07-19 16:02:44 +0800

    20190719

--- shapeshed-nodejsbook.io.examples/hour15/example05/app.js
+++ shapeshed-nodejsbook.io.examples/hour15/example05/app.js
@@ -1,18 +1,9 @@
-//var express = require('express');
-//var app = module.exports = express.createServer();
-
 const express = require('express')
-const app = module.exports = express.createServer();
-
-// var express = require("express"),
-//  app = express();
-
-//const express = require('express')
-//const app = express()
-//var http = require('http');
-  //server = http.createServer(app);
-  //*/
-
+var  app = express(),
+ errorHandler = require('errorhandler'),
+ http = require('http'),
+ server = http.createServer(app);
+  
 var rebels = [
   { name: 'Han Solo' },
   { name: 'Luke Skywalker' },
@@ -22,11 +13,13 @@ var rebels = [
   { name: 'Princess Leia' }
 ];
 
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', function(req, res, next){
   res.send(rebels);
 });
 
-app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.use(errorHandler({ dumpExceptions: true, showStack: true })); 
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+server.listen(3000,'127.0.0.1');
+console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);

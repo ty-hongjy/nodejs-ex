288959336c34415e6b4a311093e7f15acff0b742
ty-hongjy <hongjy1@163.com>
2020-08-03 09:45:26 +0800

    20200803

--- shapeshed-nodejsbook.io.examples/hour13/example03/app.js
+++ shapeshed-nodejsbook.io.examples/hour13/example03/app.js
@@ -6,15 +6,7 @@ var express = require('express'),
   nicknames = [];
 
 app.set('port', process.env.PORT || 3000);
-// app.configure(function(){
-//   app.set('views', __dirname + '/views');
-//   app.set('view engine', 'jade');
-//   app.use(express.bodyParser());
-//   app.use(express.methodOverride());
-//   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-//   app.use(app.router);
 app.use(express.static(__dirname + '/public'));
-// });
 
 if ('development' === app.get('env')) {
   app.use(errorHandler());

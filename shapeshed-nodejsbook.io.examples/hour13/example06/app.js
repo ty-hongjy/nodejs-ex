480000705788e35106805a5b89729de12ecbe7ef
ty-hongjy <hongjy1@163.com>
2021-02-07 13:06:11 +0800

    20210211

--- shapeshed-nodejsbook.io.examples/hour13/example06/app.js
+++ shapeshed-nodejsbook.io.examples/hour13/example06/app.js
@@ -33,6 +33,7 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-server.listen(app.get('port'), function(){
+// server.listen(app.get('port'), function(){
+server.listen(app.get('port'),'0.0.0.0', function(){
   console.log('Express server listening on port ' + app.get('port'));
 });

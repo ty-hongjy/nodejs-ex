fafed18ead0728ef74dafd420e43cd000c556c0a
ty-hongjy <hongjy1@163.com>
2021-02-07 13:06:46 +0800

    20210211

--- shapeshed-nodejsbook.io.examples/hour12/example04/app.js
+++ shapeshed-nodejsbook.io.examples/hour12/example04/app.js
@@ -9,7 +9,7 @@ var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(data, 'utf-8');
   });
-}).listen(3000, "127.0.0.1");
+}).listen(3000, "0.0.0.0");
 console.log('Server running at http://127.0.0.1:3000/');
 
 var io = require('socket.io').listen(server);

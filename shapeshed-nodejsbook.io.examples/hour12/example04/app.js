9bae91368ffcff285bb8a27ef82c1005e8d9699e
ty-hongjy <hongjy1@163.com>
2021-02-16 15:53:33 +0800

    20210216

--- shapeshed-nodejsbook.io.examples/hour12/example04/app.js
+++ shapeshed-nodejsbook.io.examples/hour12/example04/app.js
@@ -1,19 +1,27 @@
 var http = require('http'),
   fs = require('fs');
-  //var jsdom = require("jsdom");  
-  //var window = jsdom.jsdom().defaultView;  
-  //var $ = require('jquery');//(window); 
-var $ = require("jquery");
+const url = require('url');
+
 var server = http.createServer(function (req, res) {
-  fs.readFile('./index.html', function(error, data) {
+	fs.readFile('./index.html', function(error, data) {
+    console.log("index");
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(data, 'utf-8');
-  });
+ });
+  // fs.readFile('./js/jquery.js', function(error, data) {
+    // if(error){
+	// 	console.error("jquery",error)}
+	// console.log("jquery");
+	// // res.writeHead(200, { 'Content-Type': 'application/x-javascript'})
+    // res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.end(data, 'utf-8');
+ // });
+ 
 }).listen(3000, "0.0.0.0");
 console.log('Server running at http://127.0.0.1:3000/');
 
 var io = require('socket.io').listen(server);
-
+// var jquery.listen(server);
 io.sockets.on('connection', function (socket) {
   socket.on('ping', function (data) {
     console.log('Received PING. Sending PONG..');

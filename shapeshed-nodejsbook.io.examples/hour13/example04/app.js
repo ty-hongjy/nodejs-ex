e11df9fa3867cfcd2a647d4e853e0c9381a66872
ty-hongjy <hongjy1@163.com>
2024-10-22 13:06:55 +0800

    2024-10-22

--- shapeshed-nodejsbook.io.examples/hour13/example04/app.js
+++ shapeshed-nodejsbook.io.examples/hour13/example04/app.js
@@ -1,7 +1,7 @@
 var express = require('express'),
   app = express(),
   server = require('http').createServer(app),
-  io = require('socket.io').listen(server),
+  io = require('socket.io')().listen(server),
   errorHandler = require('errorhandler'),
   nicknames = [];
 
